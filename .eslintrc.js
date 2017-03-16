6e26149252625dd28a63ba66ec3c8322364d9c87
warnerp18 <warnerp18@gmail.com>
2017-03-16 10:36:39 -0700

    complete HW3

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "no-unused-vars": ["error", { "vars": "all", "args": "none" }],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
